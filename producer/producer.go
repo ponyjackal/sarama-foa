2334d6f8de8f458f352bd36acaa7e1125cf6a302
ponyjackal <pony1125@oulook.com>
2023-06-26 18:19:33 -0700

    feat: create comment

--- producer/producer.go
+++ producer/producer.go
@@ -1,10 +1,25 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"log"
 
 	"github.com/Shopify/sarama"
+	"github.com/gofiber/fiber/v2"
 )
+
+type Comment struct {
+	Text string `form:"text" json:"text"`
+}
+
+func main() {
+	app := fiber.New()
+	api := app.Group("/api/v1")
+
+	api.Post("/comments", createComment)
+	app.Listen(":3000")
+}
 
 func ConnectProducer(brokerUrl []string) (sarama.SyncProducer, error) {
 	config := sarama.NewConfig()
@@ -20,7 +35,7 @@ func ConnectProducer(brokerUrl []string) (sarama.SyncProducer, error) {
 	return conn, nil
 }
 
-func PushConnectToQueue(topic string, message []byte) error {
+func PushCommentToQueue(topic string, message []byte) error {
 	brokerUrl := []string{"localhost:9092"}
 	producer, err := ConnectProducer(brokerUrl)
 	if err != nil {
@@ -42,4 +57,39 @@ func PushConnectToQueue(topic string, message []byte) error {
 	fmt.Printf("Message is stored in topic(%s)/partition(%d)/offset(%d)\n", topic, partition, offset)
 
 	return nil
+}
+
+// createComment handler
+func createComment(c *fiber.Ctx) error {
+	// Instantiate new Message struct
+	cmt := new(Comment)
+
+	// Parse body into comment struct
+	if err := c.BodyParser(cmt); err != nil {
+		log.Println(err)
+		c.Status(400).JSON(&fiber.Map{
+			"success": false,
+			"message": err,
+		})
+		return err
+	}
+
+	// convert body into bytes and send it to kafka
+	cmtInBytes, err := json.Marshal(cmt)
+	PushCommentToQueue("comments", cmtInBytes)
+
+	// Return Comment in JSON format
+	err = c.JSON(&fiber.Map{
+		"success": true,
+		"message": "Comment pushed successfully",
+		"comment": cmt,
+	})
+	if err != nil {
+		c.Status(500).JSON(&fiber.Map{
+			"success": false,
+			"message": "Error creating product",
+		})
+		return err
+	}
+	return err
 }
