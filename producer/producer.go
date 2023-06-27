2088aff490d008c78109d2fa96d1b5d47876f085
ponyjackal <ponyjackal@Ponys-MacBook-Pro.local>
2023-06-26 18:04:26 -0700

    feat: connect producer

--- producer/producer.go
+++ producer/producer.go
@@ -1,3 +1,19 @@
 package main
 
-import 
+import (
+	"github.com/Shopify/sarama"
+)
+
+func ConnectProducer(brokerUrl []string) (sarama.SyncProducer, error) {
+	config := sarama.NewConfig()
+	config.Producer.Return.Successes = true
+	config.Producer.RequiredAcks = sarama.WaitForAll
+	config.Producer.Retry.Max = 5
+
+	conn, err := sarama.NewSyncProducer(brokerUrl, config)
+	if err != nil {
+		return nil, err
+	}
+
+	return conn, nil
+}
